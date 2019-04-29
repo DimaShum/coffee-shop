ae583e7cf62401670e19260fb057b37a1355b14b
shumilov <dima_rogachev39@mail.ru>
2019-04-29 10:42:17 +0300

    Improvement of project structure

--- src/main/java/by/shumilov/coffee_shop/TheCoffeeShopApplication.java
+++ src/main/java/by/shumilov/coffee_shop/TheCoffeeShopApplication.java
@@ -1,13 +1,13 @@
-package by.shumilov.backend;
+package by.shumilov.coffee_shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class TheCoffeeShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(TheCoffeeShopApplication.class, args);
 	}
 
 }
